da352993d80ead23f48992cd5e2d350b0dfb884e
Anjan Roy <hello@itzmeanjan.in>
2022-10-17 18:53:27 +0400

    test functional correctness of (i)NTT using `poly |> ntt |> intt |> poly'` & `poly == poly'`
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/test_ntt.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "ntt.hpp"
+#include <cassert>
+#include <cstring>
+
+// Test functional correctness of Dilithium PQC suite implementation
+namespace test_dilithium {
+
+// Ensure functional correctness of (inverse) NTT implementation for degree-255
+// polynomial over Z_q | q = 2^23 - 2^13 + 1, using following rule
+//
+// f -> random polynomial
+// f' = ntt(f)
+// f'' = intt(f')
+//
+// assert(f == f'')
+static void
+test_ntt_intt()
+{
+  constexpr size_t poly_len = sizeof(ff::ff_t) * ntt::N;
+
+  ff::ff_t* poly_a = static_cast<ff::ff_t*>(std::malloc(poly_len));
+  ff::ff_t* poly_b = static_cast<ff::ff_t*>(std::malloc(poly_len));
+
+  for (size_t i = 0; i < ntt::N; i++) {
+    poly_a[i] = ff::ff_t::random();
+  }
+
+  std::memcpy(poly_b, poly_a, poly_len);
+
+  ntt::ntt(poly_b);
+  ntt::intt(poly_b);
+
+  for (size_t i = 0; i < ntt::N; i++) {
+    assert(poly_a[i] == poly_b[i]);
+  }
+
+  std::free(poly_a);
+  std::free(poly_b);
+}
+
+}
--- test/main.cpp
+++ test/main.cpp
@@ -7,5 +7,8 @@ main()
   test_dilithium::test_field_ops();
   std::cout << "[test] Dilithium prime field arithmetic" << std::endl;
 
+  test_dilithium::test_ntt_intt();
+  std::cout << "[test] (i)NTT over degree-255 polynomial" << std::endl;
+
   return EXIT_SUCCESS;
 }
