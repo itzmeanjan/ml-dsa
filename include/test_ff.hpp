59fa9f01ebb67c4fe056f3b706a9c27741e53f87
Anjan Roy <hello@itzmeanjan.in>
2022-10-17 16:49:58 +0400

    test functional correctness of prime field arithmetic over Dilithium prime 2^23 - 2^13 + 1
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/test_ff.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "ff.hpp"
+#include <cassert>
+
+// Test functional correctness of Dilithium PQC suite implementation
+namespace test_dilithium {
+
+// Test functional correctness of Dilithium prime field operations, by running
+// through multiple rounds ( see template parameter ) of execution of field
+// operations on randomly generated field element
+template<const size_t rounds = 4096ul>
+static void
+test_field_ops()
+{
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<size_t> dis;
+
+  for (size_t i = 0; i < rounds; i++) {
+    const auto a = ff::ff_t::random();
+    const auto b = ff::ff_t::random();
+
+    // addition, subtraction, negation
+    const auto c = a - b;
+    const auto d = -b;
+    const auto e = a + d;
+
+    assert(c == e);
+
+    // multiplication, division, inversion
+    const auto f = a * b;
+    const auto g = f / b;
+
+    if (b == ff::ff_t::zero()) {
+      assert(g == ff::ff_t::zero());
+    } else {
+      assert(g == a);
+    }
+
+    // exponentiation, multiplication
+    const size_t exp = dis(gen);
+    const auto h = a ^ exp;
+
+    auto res = ff::ff_t::one();
+    for (size_t j = 0; j < exp; j++) {
+      res = res * a;
+    }
+
+    assert(res == h);
+  }
+}
+
+}
