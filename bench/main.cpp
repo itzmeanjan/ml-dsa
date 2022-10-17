c4506b4e40b4c2135c1d7981b86ab707a13e6e5e
Anjan Roy <hello@itzmeanjan.in>
2022-10-17 18:00:18 +0400

    benchmark exponentiation over Z_q | q = 2^23 - 2^13 + 1, with 64 -bit random exponent
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -7,6 +7,7 @@ BENCHMARK(bench_dilithium::ff_neg);
 BENCHMARK(bench_dilithium::ff_mul);
 BENCHMARK(bench_dilithium::ff_inv);
 BENCHMARK(bench_dilithium::ff_div);
+BENCHMARK(bench_dilithium::ff_exp);
 
 // benchmark runner main routine
 BENCHMARK_MAIN();
