cf29495b515048112b4025bb0fd7b0527f946775
Anjan Roy <hello@itzmeanjan.in>
2022-10-17 20:56:21 +0400

    benchmark inverse NTT of degree-255 polynomial
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -11,6 +11,7 @@ BENCHMARK(bench_dilithium::ff_exp);
 
 // register for benchmarking (i)NTT
 BENCHMARK(bench_dilithium::ntt);
+BENCHMARK(bench_dilithium::intt);
 
 // benchmark runner main routine
 BENCHMARK_MAIN();
