bba0bddba066a5e04540bbf0676156bfc7e7d37f
Anjan Roy <hello@itzmeanjan.in>
2022-10-19 17:18:28 +0400

    compress vector of polynomials using Power2Round technique
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/polyvec.hpp
+++ include/polyvec.hpp
@@ -26,4 +26,18 @@ polyvec_intt(ff::ff_t* const __restrict vec)
   }
 }
 
+// Compresses vector ( of dimension k x 1 ) of degree-255 polynomials by
+// extracting out high and low order bits
+template<const size_t k, const size_t d>
+static void
+polyvec_power2round(const ff::ff_t* const __restrict poly,
+                    ff::ff_t* const __restrict poly_hi,
+                    ff::ff_t* const __restrict poly_lo) requires(check_d(d))
+{
+  for (size_t i = 0; i < k; i++) {
+    const size_t off = i * ntt::N;
+    poly_power2round<d>(poly + off, poly_hi + off, poly_lo + off);
+  }
+}
+
 }
