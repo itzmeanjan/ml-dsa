ab158cc397f198683208aa2d2503644c4710eeb6
Anjan Roy <hello@itzmeanjan.in>
2022-10-21 09:31:47 +0400

    corrected logic of power2round routine, which is used for comrpessing public key
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/reduction.hpp
+++ include/reduction.hpp
@@ -14,22 +14,32 @@ check_d(const size_t d)
 }
 
 // Given an element of Z_q | q = 2^23 - 2^13 + 1, this routine extracts out high
-// and low order bits, using modulo of power of 2 ( = 2^d ), so that public key
-// can be compressed.
+// and low order bits s.t.
+//
+// r mod^+ q = hi * 2^D + lo | -2^{D-1} < lo <= 2^{D-1}
+//
+// so that public key can be compressed.
 //
 // See definition of this routine in figure 3 of Dilithium specification, as
 // submitted to NIST final round call
 // https://csrc.nist.gov/CSRC/media/Projects/post-quantum-cryptography/documents/round-3/submissions/Dilithium-Round3.zip
+//
+// This implementation collects some ideas from
+// https://github.com/pq-crystals/dilithium/blob/3e9b9f1/ref/rounding.c#L5-L23
 template<const size_t d>
 inline static std::pair<ff::ff_t, ff::ff_t>
 power2round(const ff::ff_t r) requires(check_d(d))
 {
-  constexpr uint32_t mask = (1u << d) - 1u;
+  constexpr uint32_t max = 1u << (d - 1);
 
-  const uint32_t t0 = r.v >> d;
-  const uint32_t t1 = r.v & mask;
+  const uint32_t t1 = r.v + max - 1u;
+  const uint32_t t2 = t1 >> d;
+  const uint32_t t3 = t2 << d;
 
-  return std::make_pair(ff::ff_t{ t0 }, ff::ff_t{ t1 });
+  const ff::ff_t hi{ t2 };
+  const ff::ff_t lo = r - ff::ff_t{ t3 };
+
+  return std::make_pair(hi, lo);
 }
 
 }
