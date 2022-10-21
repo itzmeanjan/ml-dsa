2bfe9402aa1249c4ecfcfc45bf9a96113cf2611d
Anjan Roy <hello@itzmeanjan.in>
2022-10-21 16:54:44 +0400

    added utility routine for generating random data
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,6 +6,20 @@
 // Utility functions for Dilithium Post-Quantum Digital Signature Algorithm
 namespace dilithium_utils {
 
+// Generates N -many random values of type T | N >= 0
+template<typename T>
+static inline void
+random_data(T* const data, const size_t len) requires(std::is_unsigned_v<T>)
+{
+  std::random_device rd;
+  std::mt19937_64 gen(rd());
+  std::uniform_int_distribution<T> dis;
+
+  for (size_t i = 0; i < len; i++) {
+    data[i] = dis(gen);
+  }
+}
+
 // Compile-time compute how many bytes to reserve for storing serialized
 // Dilithium public key, for given parameter set
 //
