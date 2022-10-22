ef5119a721db32ba1045089937004ba89323f2fb
Anjan Roy <hello@itzmeanjan.in>
2022-10-22 10:17:59 +0400

    benchmark dilithium key generation algorithm for three NIST security levels
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -21,11 +21,8 @@ BENCHMARK(bench_dilithium::expand_a<4, 4>);
 BENCHMARK(bench_dilithium::expand_a<6, 5>);
 BENCHMARK(bench_dilithium::expand_a<8, 7>);
 BENCHMARK(bench_dilithium::uniform_sampling_eta<2, 4>);
-BENCHMARK(bench_dilithium::uniform_sampling_eta<2, 6>);
 BENCHMARK(bench_dilithium::uniform_sampling_eta<2, 8>);
-BENCHMARK(bench_dilithium::uniform_sampling_eta<4, 4>);
 BENCHMARK(bench_dilithium::uniform_sampling_eta<4, 6>);
-BENCHMARK(bench_dilithium::uniform_sampling_eta<4, 8>);
 
 // register for benchmarking serialization/ deserialization of polynomials
 BENCHMARK(bench_dilithium::encode<3>);
@@ -36,6 +33,11 @@ BENCHMARK(bench_dilithium::encode<10>);
 BENCHMARK(bench_dilithium::decode<10>);
 BENCHMARK(bench_dilithium::encode<13>);
 BENCHMARK(bench_dilithium::decode<13>);
+
+// register for benchmarking Dilithium Key Generation Algorithm
+BENCHMARK(bench_dilithium::keygen<4, 4, 2, 13>); // NIST security level 2
+BENCHMARK(bench_dilithium::keygen<6, 5, 4, 13>); // NIST security level 3
+BENCHMARK(bench_dilithium::keygen<8, 7, 2, 13>); // NIST security level 5
 
 // benchmark runner main routine
 BENCHMARK_MAIN();
---
+++ include/bench_keygen.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "keygen.hpp"
+#include "utils.hpp"
+#include <benchmark/benchmark.h>
+
+// Benchmark Dilithium PQC suite implementation on CPU, using google-benchmark
+namespace bench_dilithium {
+
+// Benchmark Dilithium Key Generation Algorithm, for different parameter sets
+template<const size_t k, const size_t l, const uint32_t eta, const size_t d>
+void
+keygen(benchmark::State& state)
+{
+  constexpr size_t slen = 32;
+  constexpr size_t pklen = dilithium_utils::pubkey_length<k, d>();
+  constexpr size_t sklen = dilithium_utils::seckey_length<k, l, eta, d>();
+
+  uint8_t* seed = static_cast<uint8_t*>(std::malloc(slen));
+  uint8_t* pubkey = static_cast<uint8_t*>(std::malloc(pklen));
+  uint8_t* seckey = static_cast<uint8_t*>(std::malloc(sklen));
+
+  dilithium_utils::random_data<uint8_t>(seed, slen);
+
+  for (auto _ : state) {
+    dilithium::keygen<k, l, eta, d>(seed, pubkey, seckey);
+
+    benchmark::DoNotOptimize(seed);
+    benchmark::DoNotOptimize(pubkey);
+    benchmark::DoNotOptimize(seckey);
+    benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(static_cast<int64_t>(state.iterations()));
+
+  std::free(seed);
+  std::free(pubkey);
+  std::free(seckey);
+}
+
+}
