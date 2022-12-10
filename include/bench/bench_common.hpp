3b5c65162669921b90b041db93b2ae95e5523662
Anjan Roy <hello@itzmeanjan.in>
2022-12-10 17:00:35 +0400

    common benchmark/ timing related dependencies
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

---
+++ include/bench/bench_common.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <algorithm>
+#include <benchmark/benchmark.h>
+#include <chrono>
+#include <vector>
+
+// Benchmark Dilithium PQC suite implementation on CPU, using google-benchmark
+namespace bench_dilithium {
+
+// Alias for casting duration to second level granularity, used for manual
+// timing using chrono high resolution clock
+using seconds_t = std::chrono::duration<double>;
+// Alias for casting duration to nanosecond level granularity, used for timing
+// minimum, median and maximum execution times
+using nano_t = std::chrono::nanoseconds;
+
+}
