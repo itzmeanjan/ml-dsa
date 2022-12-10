686e07929e41aee87b3d7cdfae2d6243fda2b7f3
Anjan Roy <hello@itzmeanjan.in>
2022-12-10 16:58:11 +0400

    manually time execution of Dilithium key generation ( min, median, mean & max cases are measured )
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/bench/bench_keygen.hpp
+++ include/bench/bench_keygen.hpp
@@ -1,8 +1,8 @@
 #pragma once
+#include "bench_common.hpp"
 #include "keygen.hpp"
 #include "utils.hpp"
 #include "x86_64_cpu_cycles.hpp"
-#include <benchmark/benchmark.h>
 
 // Benchmark Dilithium PQC suite implementation on CPU, using google-benchmark
 namespace bench_dilithium {
@@ -26,7 +26,11 @@ keygen(benchmark::State& state)
   uint64_t total_cycles = 0ul;
 #endif
 
+  std::vector<uint64_t> durations;
+
   for (auto _ : state) {
+    const auto t0 = std::chrono::high_resolution_clock::now();
+
 #if defined __x86_64__
     const uint64_t start = cpu_cycles();
 #endif
@@ -38,6 +42,14 @@ keygen(benchmark::State& state)
     total_cycles += (end - start);
 #endif
 
+    const auto t1 = std::chrono::high_resolution_clock::now();
+
+    const auto sdur = std::chrono::duration_cast<seconds_t>(t1 - t0);
+    const auto nsdur = std::chrono::duration_cast<nano_t>(t1 - t0);
+
+    state.SetIterationTime(sdur.count());
+    durations.push_back(nsdur.count());
+
     benchmark::DoNotOptimize(seed);
     benchmark::DoNotOptimize(pubkey);
     benchmark::DoNotOptimize(seckey);
@@ -45,6 +57,20 @@ keygen(benchmark::State& state)
   }
 
   state.SetItemsProcessed(static_cast<int64_t>(state.iterations()));
+
+  const auto min_idx = std::min_element(durations.begin(), durations.end());
+  const auto min = durations.at(std::distance(durations.begin(), min_idx));
+  state.counters["min_exec_time (ns)"] = static_cast<double>(min);
+
+  const auto max_idx = std::max_element(durations.begin(), durations.end());
+  const auto max = durations.at(std::distance(durations.begin(), max_idx));
+  state.counters["max_exec_time (ns)"] = static_cast<double>(max);
+
+  const auto lenby2 = durations.size() / 2;
+  const auto mid_idx = durations.begin() + lenby2;
+  std::nth_element(durations.begin(), mid_idx, durations.end());
+  const auto mid = durations[lenby2];
+  state.counters["median_exec_time (ns)"] = static_cast<double>(mid);
 
 #if defined __x86_64__
   total_cycles /= static_cast<uint64_t>(state.iterations());
