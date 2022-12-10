7530a324942d79d0c41d88ef4eedc34222fc58e0
Anjan Roy <hello@itzmeanjan.in>
2022-12-10 17:00:59 +0400

    use manual timing for benchmarking Dilithium-{2,3,5} keygen/ sign/ verify
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- bench/main.cpp
+++ bench/main.cpp
@@ -3,15 +3,27 @@
 // register for benchmarking Dilithium Key Generation, Signing & Verification
 using namespace bench_dilithium;
 
-BENCHMARK(keygen<4, 4, 13, 2>)->Arg(32);
-BENCHMARK(sign<4, 4, 13, 2, 131072, 95232, 39, 78, 80>)->Arg(32);
-BENCHMARK(verify<4, 4, 13, 2, 131072, 95232, 39, 78, 80>)->Arg(32);
-BENCHMARK(keygen<6, 5, 13, 4>)->Arg(32);
-BENCHMARK(sign<6, 5, 13, 4, 524288, 261888, 49, 196, 55>)->Arg(32);
-BENCHMARK(verify<6, 5, 13, 4, 524288, 261888, 49, 196, 55>)->Arg(32);
-BENCHMARK(keygen<8, 7, 13, 2>)->Arg(32);
-BENCHMARK(sign<8, 7, 13, 2, 524288, 261888, 60, 120, 75>)->Arg(32);
-BENCHMARK(verify<8, 7, 13, 2, 524288, 261888, 60, 120, 75>)->Arg(32);
+BENCHMARK(keygen<4, 4, 13, 2>)->UseManualTime();
+BENCHMARK(sign<4, 4, 13, 2, 131072, 95232, 39, 78, 80>)
+  ->Arg(32)
+  ->UseManualTime();
+BENCHMARK(verify<4, 4, 13, 2, 131072, 95232, 39, 78, 80>)
+  ->Arg(32)
+  ->UseManualTime();
+BENCHMARK(keygen<6, 5, 13, 4>)->UseManualTime();
+BENCHMARK(sign<6, 5, 13, 4, 524288, 261888, 49, 196, 55>)
+  ->Arg(32)
+  ->UseManualTime();
+BENCHMARK(verify<6, 5, 13, 4, 524288, 261888, 49, 196, 55>)
+  ->Arg(32)
+  ->UseManualTime();
+BENCHMARK(keygen<8, 7, 13, 2>)->UseManualTime();
+BENCHMARK(sign<8, 7, 13, 2, 524288, 261888, 60, 120, 75>)
+  ->Arg(32)
+  ->UseManualTime();
+BENCHMARK(verify<8, 7, 13, 2, 524288, 261888, 60, 120, 75>)
+  ->Arg(32)
+  ->UseManualTime();
 
 // benchmark runner main routine
 BENCHMARK_MAIN();
