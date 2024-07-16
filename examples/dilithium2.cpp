9f26338f49554c48c155a2488460d28dd49ce9c3
Anjan Roy <hello@itzmeanjan.in>
2024-07-16 18:28:53 +0400

    Refactor the example program, ensuring it works with recent API changes
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- examples/dilithium2.cpp
+++ examples/dilithium2.cpp
@@ -1,8 +1,23 @@
-#include "dilithium2.hpp"
-#include "prng.hpp"
+#include "ml_dsa/dilithium2.hpp"
+#include "ml_dsa/internals/rng/prng.hpp"
 #include <cassert>
 #include <iostream>
 #include <vector>
+
+// Given a bytearray of length N, this function converts it to human readable
+// hex string of length N << 1 | N >= 0
+static inline const std::string
+to_hex(std::span<const uint8_t> bytes)
+{
+  std::stringstream ss;
+  ss << std::hex;
+
+  for (size_t i = 0; i < bytes.size(); i++) {
+    ss << std::setw(2) << std::setfill('0') << static_cast<uint32_t>(bytes[i]);
+  }
+
+  return ss.str();
+}
 
 // Compile it with
 //
@@ -14,12 +29,14 @@ main()
   constexpr size_t mlen = 32; // message length ( can be anything >= 1 )
 
   std::vector<uint8_t> seed(slen);
+  std::vector<uint8_t> rnd(slen);
   std::vector<uint8_t> msg(mlen);
   std::vector<uint8_t> pubkey(dilithium2::PubKeyLen);
   std::vector<uint8_t> seckey(dilithium2::SecKeyLen);
   std::vector<uint8_t> sig(dilithium2::SigLen);
 
   auto _seed = std::span<uint8_t, slen>(seed);
+  auto _rnd = std::span<uint8_t, slen>(rnd);
   auto _msg = std::span(msg);
   auto _pubkey = std::span<uint8_t, dilithium2::PubKeyLen>(pubkey);
   auto _seckey = std::span<uint8_t, dilithium2::SecKeyLen>(seckey);
@@ -31,23 +48,17 @@ main()
   prng.read(_seed);
   prng.read(_msg);
 
-  bool flg = false;
-
   // Key generation -> signing -> verification
   dilithium2::keygen(_seed, _pubkey, _seckey);
-  dilithium2::sign(_seckey, _msg, _sig, {});
-
-  // Default option is deterministic signing, for randomized signing, use
-  // dilithium2::sign<true>(_seckey, _msg, _sig, std::array<uint8_t, 64>{});
-
-  flg = dilithium2::verify(_pubkey, _msg, _sig);
+  dilithium2::sign(_rnd, _seckey, _msg, _sig);
+  const bool flg = dilithium2::verify(_pubkey, _msg, _sig);
 
   std::cout << "Dilithium @ NIST security level 2\n";
-  std::cout << "seed      : " << dilithium_utils::to_hex(_seed) << "\n";
-  std::cout << "pubkey    : " << dilithium_utils::to_hex(_pubkey) << "\n";
-  std::cout << "seckey    : " << dilithium_utils::to_hex(_seckey) << "\n";
-  std::cout << "message   : " << dilithium_utils::to_hex(_msg) << "\n";
-  std::cout << "signature : " << dilithium_utils::to_hex(_sig) << "\n";
+  std::cout << "seed      : " << to_hex(_seed) << "\n";
+  std::cout << "pubkey    : " << to_hex(_pubkey) << "\n";
+  std::cout << "seckey    : " << to_hex(_seckey) << "\n";
+  std::cout << "message   : " << to_hex(_msg) << "\n";
+  std::cout << "signature : " << to_hex(_sig) << "\n";
   std::cout << "verified   : " << std::boolalpha << flg << "\n";
 
   // check that signature verification passed !
