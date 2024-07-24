8d95572ebe242cc926b8f27a15c209b2df0a8263
Anjan Roy <hello@itzmeanjan.in>
2024-07-24 21:03:04 +0400

    Update API usage example
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- examples/ml_dsa_44.cpp
+++ examples/ml_dsa_44.cpp
@@ -4,9 +4,8 @@
 #include <iostream>
 #include <vector>
 
-// Given a bytearray of length N, this function converts it to human readable
-// hex string of length N << 1 | N >= 0
-static inline const std::string
+// Given a bytearray of length N, this function converts it to human readable hex string of length 2*N | N >= 0.
+static inline std::string
 to_hex(std::span<const uint8_t> bytes)
 {
   std::stringstream ss;
@@ -21,49 +20,48 @@ to_hex(std::span<const uint8_t> bytes)
 
 // Compile it with
 //
-// g++ -std=c++20 -O3 -march=native -I ./include -I ./sha3/include examples/ml_dsa_44.cpp
+// g++ -std=c++20 -Wall -Wextra -pedantic -O3 -march=native -I ./include -I ./sha3/include examples/ml_dsa_44.cpp
 int
 main()
 {
-  constexpr size_t slen = 32; // seed length
-  constexpr size_t mlen = 32; // message length ( can be anything >= 1 )
+  // Byte length of message, can be anything >= 0.
+  constexpr size_t mlen = 32;
 
-  std::vector<uint8_t> seed(slen);
-  std::vector<uint8_t> rnd(slen);
+  std::vector<uint8_t> seed(ml_dsa_44::KeygenSeedByteLen);
+  std::vector<uint8_t> pubkey(ml_dsa_44::PubKeyByteLen);
+  std::vector<uint8_t> seckey(ml_dsa_44::SecKeyByteLen);
+  std::vector<uint8_t> rnd(ml_dsa_44::SigningSeedByteLen);
   std::vector<uint8_t> msg(mlen);
-  std::vector<uint8_t> pubkey(ml_dsa_44::PubKeyLen);
-  std::vector<uint8_t> seckey(ml_dsa_44::SecKeyLen);
-  std::vector<uint8_t> sig(ml_dsa_44::SigLen);
+  std::vector<uint8_t> sig(ml_dsa_44::SigByteLen);
 
-  auto _seed = std::span<uint8_t, slen>(seed);
-  auto _rnd = std::span<uint8_t, slen>(rnd);
-  auto _msg = std::span(msg);
-  auto _pubkey = std::span<uint8_t, ml_dsa_44::PubKeyLen>(pubkey);
-  auto _seckey = std::span<uint8_t, ml_dsa_44::SecKeyLen>(seckey);
-  auto _sig = std::span<uint8_t, ml_dsa_44::SigLen>(sig);
+  auto seed_span = std::span<uint8_t, ml_dsa_44::KeygenSeedByteLen>(seed);
+  auto rnd_span = std::span<uint8_t, ml_dsa_44::SigningSeedByteLen>(rnd);
+  auto msg_span = std::span(msg);
+  auto pubkey_span = std::span<uint8_t, ml_dsa_44::PubKeyByteLen>(pubkey);
+  auto seckey_span = std::span<uint8_t, ml_dsa_44::SecKeyByteLen>(seckey);
+  auto sig_span = std::span<uint8_t, ml_dsa_44::SigByteLen>(sig);
 
-  // Generate random 32 -bytes seed, 32 -bytes randomness (for signing) and N -bytes message ( to be signed )
   ml_dsa_prng::prng_t<128> prng;
 
-  prng.read(_seed);
-  prng.read(_rnd);
-  prng.read(_msg);
+  prng.read(seed_span);
+  prng.read(msg_span);
 
-  // Key generation -> signing -> verification
-  ml_dsa_44::keygen(_seed, _pubkey, _seckey);
-  ml_dsa_44::sign(_rnd, _seckey, _msg, _sig);
-  const bool flg = ml_dsa_44::verify(_pubkey, _msg, _sig);
+  // Filling `rnd` with random bytes, invokes default and recommended "hedged" version of signing.
+  // While filling it with 0, invokes "deterministic" signing mode.
+  prng.read(rnd_span);
 
-  std::cout << "Dilithium @ NIST security level 2\n";
-  std::cout << "seed      : " << to_hex(_seed) << "\n";
-  std::cout << "pubkey    : " << to_hex(_pubkey) << "\n";
-  std::cout << "seckey    : " << to_hex(_seckey) << "\n";
-  std::cout << "message   : " << to_hex(_msg) << "\n";
-  std::cout << "signature : " << to_hex(_sig) << "\n";
-  std::cout << "verified   : " << std::boolalpha << flg << "\n";
+  ml_dsa_44::keygen(seed_span, pubkey_span, seckey_span);
+  ml_dsa_44::sign(rnd_span, seckey_span, msg_span, sig_span);
+  const bool is_valid = ml_dsa_44::verify(pubkey_span, msg_span, sig_span);
 
-  // check that signature verification passed !
-  assert(flg);
+  std::cout << "ML-DSA-44 @ NIST security level 2\n";
+  std::cout << "Seed      : " << to_hex(seed_span) << "\n";
+  std::cout << "Pubkey    : " << to_hex(pubkey_span) << "\n";
+  std::cout << "Seckey    : " << to_hex(seckey_span) << "\n";
+  std::cout << "Message   : " << to_hex(msg_span) << "\n";
+  std::cout << "Signature : " << to_hex(sig_span) << "\n";
+  std::cout << "Verified   : " << std::boolalpha << is_valid << "\n";
 
+  assert(is_valid);
   return EXIT_SUCCESS;
 }
