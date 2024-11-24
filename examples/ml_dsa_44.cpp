95f2437b7f26788c835f08f95163b54381693fe5
Anjan Roy <hello@itzmeanjan.in>
2024-11-24 19:45:45 +0400

    Update example program so that it works with updated sign/ verify public API
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- examples/ml_dsa_44.cpp
+++ examples/ml_dsa_44.cpp
@@ -19,25 +19,25 @@ to_hex(std::span<const uint8_t> bytes)
   return ss.str();
 }
 
-// Compile it with
-//
-// g++ -std=c++20 -Wall -Wextra -pedantic -O3 -march=native -I ./include -I ./sha3/include -I ./RandomShake/include examples/ml_dsa_44.cpp
 int
 main()
 {
   // Byte length of message, can be anything >= 0.
   constexpr size_t mlen = 32;
+  constexpr size_t ctx_len = 8;
 
   std::vector<uint8_t> seed(ml_dsa_44::KeygenSeedByteLen);
   std::vector<uint8_t> pubkey(ml_dsa_44::PubKeyByteLen);
   std::vector<uint8_t> seckey(ml_dsa_44::SecKeyByteLen);
   std::vector<uint8_t> rnd(ml_dsa_44::SigningSeedByteLen);
   std::vector<uint8_t> msg(mlen);
+  std::vector<uint8_t> ctx(ctx_len);
   std::vector<uint8_t> sig(ml_dsa_44::SigByteLen);
 
   auto seed_span = std::span<uint8_t, ml_dsa_44::KeygenSeedByteLen>(seed);
   auto rnd_span = std::span<uint8_t, ml_dsa_44::SigningSeedByteLen>(rnd);
   auto msg_span = std::span(msg);
+  auto ctx_span = std::span(ctx);
   auto pubkey_span = std::span<uint8_t, ml_dsa_44::PubKeyByteLen>(pubkey);
   auto seckey_span = std::span<uint8_t, ml_dsa_44::SecKeyByteLen>(seckey);
   auto sig_span = std::span<uint8_t, ml_dsa_44::SigByteLen>(sig);
@@ -46,23 +46,27 @@ main()
 
   csprng.generate(seed_span);
   csprng.generate(msg_span);
+  csprng.generate(ctx_span);
 
   // Filling `rnd` with random bytes, invokes default and recommended "hedged" version of signing.
   // While filling it with 0, invokes "deterministic" signing mode.
   csprng.generate(rnd_span);
 
   ml_dsa_44::keygen(seed_span, pubkey_span, seckey_span);
-  ml_dsa_44::sign(rnd_span, seckey_span, msg_span, sig_span);
-  const bool is_valid = ml_dsa_44::verify(pubkey_span, msg_span, sig_span);
+  const bool has_signed = ml_dsa_44::sign(rnd_span, seckey_span, msg_span, ctx_span, sig_span);
+  const bool is_valid = ml_dsa_44::verify(pubkey_span, msg_span, ctx_span, sig_span);
 
   std::cout << "ML-DSA-44 @ NIST security level 2\n";
   std::cout << "Seed      : " << to_hex(seed_span) << "\n";
   std::cout << "Pubkey    : " << to_hex(pubkey_span) << "\n";
   std::cout << "Seckey    : " << to_hex(seckey_span) << "\n";
   std::cout << "Message   : " << to_hex(msg_span) << "\n";
+  std::cout << "Context   : " << to_hex(ctx_span) << "\n";
   std::cout << "Signature : " << to_hex(sig_span) << "\n";
   std::cout << "Verified   : " << std::boolalpha << is_valid << "\n";
 
+  assert(has_signed);
   assert(is_valid);
+
   return EXIT_SUCCESS;
 }
