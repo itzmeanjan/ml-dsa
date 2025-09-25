bd4b7bcdd29448667a8e394a3f4157c5e340a754
Anjan Roy <hello@itzmeanjan.in>
2025-09-25 12:59:02 +0530

    Add script for parsing and extracting ML-DSA signature-verification KATs from ACVP Server
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- kats/scripts/sync_acvp_kats.sh
+++ kats/scripts/sync_acvp_kats.sh
@@ -16,6 +16,8 @@ fi
 
 cat ./ACVP-Server/gen-val/json-files/ML-DSA-keyGen-FIPS204/internalProjection.json | python parse_ml_dsa_keygen_acvp_kat.py
 cat ./ACVP-Server/gen-val/json-files/ML-DSA-sigGen-FIPS204/internalProjection.json | python parse_ml_dsa_sign_acvp_kat.py
+cat ./ACVP-Server/gen-val/json-files/ML-DSA-sigVer-FIPS204/internalProjection.json | python parse_ml_dsa_verify_acvp_kat.py
+
 mv *.acvp.kat ..
 
 echo "> Generated all NIST ACVP KATs."
